d903dfb4b8bf237b2e7ebf98057709d9c7fee470
Allen D. Householder <ahouseholder@users.noreply.github.com>
2022-02-24 12:59:23 -0500

    fix typo

--- labyrinth/patterns.py
+++ labyrinth/patterns.py
@@ -152,7 +152,7 @@ def normalize(id_str):
 
     m = re.match("GHSA[^a-z0-9]*([a-z0-9]+)[^a-z0-9]*([a-z0-9]+)[^a-z0-9]*([a-z0-9]+)", id_str, re.IGNORECASE)
     if m:
-        return f"GHSA-{m.groups()[0])-{m.groups()[1]}-{m.groups()[2]}"
+        return f"GHSA-{m.groups()[0]}-{m.groups()[1]}-{m.groups()[2]}"
 
     # default to no change
     return id_str
